fd9b28f9a438cbf5607ba19f3c401bc2b5222cda
poojasaxena2000 <poojags2805@gmail.com>
2025-05-11 19:34:34 +0530

    Fixed initial render for 'Full Name Display' and adjusted form validation

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -49,13 +49,14 @@ function Homepage() {
             Submit
           </button>
         </form>
-        {fullName && (
-          <div className="mt-4 p-4 bg-green-100 text-green-700 rounded-lg">
-            Full Name: {fullName}
-          </div>
-        )}
+
+        {/* Display 'Full Name Display' on initial render and update with user input */}
+        <div className="mt-4 p-4 bg-gray-100 text-gray-700 rounded-lg">
+          Full Name Display: {fullName || "N/A"}
+        </div>
       </div>
     </div>
   );
 }
-export default  Homepage
+
+export default Homepage;
